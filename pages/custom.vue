30e3b2fee78b56e282612f21fc540f6903862fa7
SAmmmg <plupvc90@163.com>
2025-02-05 14:40:45 +0800

    测试定制2

--- pages/custom.vue
+++ pages/custom.vue
@@ -4,11 +4,11 @@
             <!-- #region 定制鼓棒 -->
             <div class="custom-gb">
                 <div>
-                    <button class="active">左棒</button>
+                    <button @click="bang = 'left'" :class="{ active: bang == 'left' }">左棒</button>
                     <canvas ref="left" width="1024" height="74"></canvas>
                 </div>
                 <div>
-                    <button>右棒</button>
+                    <button @click="bang = 'right'" :class="{ active: bang == 'right' }">右棒</button>
                     <canvas ref="right" width="1024" height="74"></canvas>
                 </div>
             </div>
@@ -49,20 +49,40 @@
                         </div>
 
                         <el-collapse accordion>
-                            <el-collapse-item v-for="el in 2" title="A区" :name="el">
+                            <el-collapse-item title="A区">
                                 <template #icon="{ isActive }">
                                     <img v-show="isActive" style="margin-left: auto; width: 20px" src="/assets/Group 308@2x.png" alt="" />
                                     <img v-show="!isActive" style="margin-left: auto; width: 20px" src="/assets/Group 300@2x.png" alt="" />
                                 </template>
                                 <div class="ipts">
-                                    <el-select v-model="ac" placeholder="选择定制字体">
+                                    <el-select v-model="form.fontFamily" placeholder="选择定制字体">
                                         <el-option v-for="item in 3" :key="item" :label="item" :value="item" />
                                     </el-select>
-                                    <el-input placeholder="请输入文字内容" v-model="leftForm.txt" @blur="onBlur" />
-                                    <el-select v-model="ac" placeholder="选择定制图标">
-                                        <el-option v-for="item in 3" :key="item" :label="item" :value="item" />
+                                    <el-input placeholder="请输入文字内容" v-model="form.txt" />
+                                    <el-select v-model="form.icon" placeholder="选择定制图标" clearable>
+                                        <!-- <el-option v-for="item in 3" :key="item" :label="item" :value="item" /> -->
+
+                                        <template #label>
+                                            <img v-show="form.icon" :style="{ width: '20px' }" :src="form.icon" alt="" />
+                                            <!-- <span>阿弥诺斯</span> -->
+                                            <!-- <el-tag v-for="color in value" :key="color" :color="color" /> -->
+                                        </template>
                                     </el-select>
+                                    <CustomBtn txt="上传我的图标" :active="true" />
+                                    <el-upload action="#" :before-upload="beforeUpload" :auto-upload="true">上传我的图标</el-upload>
                                 </div>
+                                <el-tabs type="border-card">
+                                    <el-tab-pane label="十二星座">
+                                        <div class="icons">
+                                            <img
+                                                @click="form.icon = el"
+                                                v-for="el in ['/image/Frame@2x(1).png', '/image/Frame@2x(2).png']"
+                                                :src="el"
+                                            />
+                                        </div>
+                                    </el-tab-pane>
+                                    <el-tab-pane label="十二生肖">Config</el-tab-pane>
+                                </el-tabs>
                             </el-collapse-item>
                         </el-collapse>
                     </div>
@@ -91,53 +111,111 @@
                 </div>
             </div>
         </div>
-
-        <!-- <canvas style="visibility: hidden"></canvas> -->
     </div>
 </template>
 
 <script setup lang="ts">
-const ac = ref(1);
+const bang = ref<"left" | "right">("left");
 const left = ref<HTMLCanvasElement>();
 const right = ref<HTMLCanvasElement>();
 
 let leftCtx: CanvasRenderingContext2D;
 let rightCtx: CanvasRenderingContext2D;
 
-const leftForm = reactive({
-    txt: "",
-});
-const rightForm = reactive({
-    txt: "",
-});
-
-function onBlur() {
-    leftCtx.font = "24px 'Songti TC'";
-    leftCtx.fillText(leftForm.txt, 300, 40);
-
-    let image = new Image();
-    image.onload = () => {
-        let w = image.width;
-        let h = image.height;
-        // console.log();
-        leftCtx.drawImage(image, 500, 17, w * (38 / h), 38);
-        // rightCtx.drawImage(image, 0, 0, 1024, 74);
-    };
-    image.src = "/image/logo.png";
+interface form {
+    txt: string;
+    fontFamily: string;
+    // 预设图标
+    icon: string;
+    // 用户定制图标
+    // customIcon: string;
 }
+const leftForm = reactive<form>({
+    txt: "",
+    fontFamily: "",
+    icon: "",
+});
+const rightForm = reactive<form>({
+    txt: "",
+    fontFamily: "",
+    icon: "",
+});
+const form = computed<form>(() => {
+    return bang.value == "left" ? leftForm : rightForm;
+});
+
+let timer: NodeJS.Timeout;
+watch(leftForm, () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        leftCtx.clearRect(0, 0, 1024, 74);
+
+        drawBot("left", () => {
+            leftCtx.font = "24px 'Songti TC'";
+            leftCtx.fillText(leftForm.txt, 300, 44);
+
+            let image = new Image();
+            image.onload = () => {
+                let w = image.width;
+                let h = image.height;
+                leftCtx.drawImage(image, 500, 17, w * (38 / h), 38);
+            };
+            image.src = form.value.icon;
+        });
+    }, 1000);
+});
+watch(rightForm, () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        rightCtx.clearRect(0, 0, 1024, 74);
+
+        drawBot("right", () => {
+            rightCtx.font = "24px 'Songti TC'";
+            rightCtx.fillText(leftForm.txt, 300, 44);
+
+            let image = new Image();
+            image.onload = () => {
+                let w = image.width;
+                let h = image.height;
+                rightCtx.drawImage(image, 500, 17, w * (38 / h), 38);
+            };
+            image.src = "/image/logo@2x.png";
+        });
+    }, 1000);
+});
+
 onMounted(() => {
     leftCtx = left.value?.getContext("2d") as CanvasRenderingContext2D;
     rightCtx = right.value?.getContext("2d") as CanvasRenderingContext2D;
 
+    drawBot("left");
+    drawBot("right");
+});
+function drawBot(type: "left" | "right", callBack?: Function) {
     let image = new Image();
     image.onload = () => {
-        leftCtx.drawImage(image, 0, 0, 1024, 74);
-        rightCtx.drawImage(image, 0, 0, 1024, 74);
+        if (type == "left") {
+            leftCtx.drawImage(image, 0, 0, 1024, 74);
+        } else {
+            rightCtx.drawImage(image, 0, 0, 1024, 74);
+        }
+        if (callBack) callBack();
     };
     image.src = "/image/鼓棒@2x.png";
-    // console.log(left);
-    // console.log(right);
-});
+}
+
+function beforeUpload(file: File) {
+    // console.log(raw);
+
+    const reader = new FileReader();
+
+    reader.onload = function () {
+        form.value.icon = reader.result as string;
+        console.log(reader.result); // 输出 Base64 编码字符串
+    };
+    // 读取文件内容并转换为 Base64 编码字符串
+    return false;
+}
 </script>
 
 <style lang="scss" scoped>
@@ -212,7 +290,7 @@ onMounted(() => {
                 background-color: var(--main-color-3);
                 padding: 20px;
                 :deep(.el-collapse) {
-                    button,
+                    button:not(.custom-btn),
                     div {
                         background-color: transparent !important;
                     }
@@ -239,8 +317,26 @@ onMounted(() => {
                     display: flex;
                     gap: 10px;
                     flex-wrap: wrap;
+                    margin-bottom: 10px;
                     & > div {
                         width: calc(50% - 5px);
+                    }
+                    .custom-btn {
+                        width: 120px;
+                    }
+                }
+                .icons {
+                    display: flex;
+                    flex-wrap: wrap;
+                    gap: 10px;
+                    margin-right: -10px;
+
+                    img {
+                        cursor: pointer;
+                        width: 20px;
+                        &:hover {
+                            background-color: rgba($color: #000, $alpha: 0.1);
+                        }
                     }
                 }
             }
