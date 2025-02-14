1c58fc0fa3e0a2bdc31bc4dbdc56abb1ce545c0a
SAmmmg <plupvc90@163.com>
2025-02-14 11:48:07 +0800

    添加h5我的用户名页

---
+++ pages/myName.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="min-h-[calc(100vh-40px)] flex flex-col">
+        <h5-Back class="p-[16px] bg-white" title="我的用户名" />
+        <van-cell-group>
+            <van-field v-model="name" placeholder="昵称">
+                <template #extra>
+                    <span class="ml-[10px]">AAD更好</span>
+                </template>
+            </van-field>
+        </van-cell-group>
+    </div>
+</template>
+
+<script setup lang="ts">
+const name = ref();
+</script>
+
+<style lang="scss" scoped></style>
