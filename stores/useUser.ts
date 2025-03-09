16d42e1484956132431c1ef79d99376f88fdc883
SAmmmg <plupvc90@163.com>
2025-03-09 15:04:57 +0800

    初始获取用户名

--- stores/useUser.ts
+++ stores/useUser.ts
@@ -3,13 +3,20 @@ import { defineStore } from "pinia";
 export const useUser = defineStore("user", () => {
     const isMobile = ref(false);
     const userInfo = ref({
-        username: localStorage.getItem("username") || "",
-        password: localStorage.getItem("password") || "",
-        expire: localStorage.getItem("expire") || "",
-        // token: localStorage.getItem("token") || ""
+        username: "",
+        password: "",
+        expire: "",
+    });
+
+    onMounted(() => {
+        Object.assign(userInfo, {
+            username: localStorage.getItem("username") || "",
+            password: localStorage.getItem("password") || "",
+            expire: localStorage.getItem("expire") || "",
+        });
     });
     return {
         isMobile,
-        userInfo
+        userInfo,
     };
 });
