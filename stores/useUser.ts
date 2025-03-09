e74b3a209c492f23ca816bcf378019b2a44e0bf4
liuzeng6 <2186184487@qq.com>
2025-03-09 14:44:25 +0800

    提交代码

--- stores/useUser.ts
+++ stores/useUser.ts
@@ -2,7 +2,14 @@ import { defineStore } from "pinia";
 
 export const useUser = defineStore("user", () => {
     const isMobile = ref(false);
+    const userInfo = ref({
+        username: localStorage.getItem("username") || "",
+        password: localStorage.getItem("password") || "",
+        expire: localStorage.getItem("expire") || "",
+        // token: localStorage.getItem("token") || ""
+    });
     return {
         isMobile,
+        userInfo
     };
 });
